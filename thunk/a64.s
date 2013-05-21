8e6977baae4242e68f6d8724dd016ce21df7dd05
7shi <7shi@live.jp>
2013-05-21 22:31:20 +0900

    サンクでABI変換

--- thunk/a64.s
+++ thunk/a64.s
@@ -1,13 +1,33 @@
 .intel_syntax noprefix
-pop rax
-pop r10
-push rcx
-mov rcx, r10
-sub rsp, 32
-call rax
-add rsp, 32
-pop rcx
-test rax, rax
-jz 0f
-jmp rax
-0: ret
+
+.globl elfstart
+elfstart:
+	push rbp
+	push rsi
+	push rdi
+	call rcx
+	pop rdi
+	pop rsi
+	pop rbp
+	ret
+
+.globl interp
+interp:
+	pop rcx
+	pop rdx
+	sub rsp, 40
+	call rcx
+	add rsp, 40
+	test rax, rax
+	jz 0f
+	jmp rax
+0:	ret
+
+.globl thunk
+thunk:
+	movabs rax, 0
+	mov rcx, rdi
+	sub rsp, 40
+	call rax
+	add rsp, 40
+	ret
